b8c430cb64e5b76c66ce914b2624ed3514d2550d
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-12 18:35:57 +0530

    conversion logic and views - in progress

--- mysite/convert/views.py
+++ mysite/convert/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from django.http import Http404
+from .forms import CurrencyConvertForm
+from .models import Currencies
 
 
 # Create your views here.
 def home(request):
     """use form on home page to get input and output currecies"""
-    return render(request, template_name='/converter/home.html')
+
+    if request.method == 'POST':
+        form = CurrencyConvertForm(request.POST)
+        if form.is_valid():
+            input_curr = form.cleaned_data['input_currency']
+            output_curr = form.cleaned_data['output_currency']
+            # currency conversion logic
+            # get db value for given -> USD
+            conversion_to_usd = Currencies.object.all()
+            # get db value for USD -> output
+            # input_currency
+            # input_value
+            # output_currency
+            # output_value
+            return render(request, template_name='/converter/home.html',
+                          context={'form': form})
+
+    form = CurrencyConvertForm()
+
+    return render(request, template_name='/converter/home.html',
+                  context={'form': form})
