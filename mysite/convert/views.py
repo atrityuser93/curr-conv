6912abed8ef60038c2618e4e82415d4d5a11daec
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-14 10:48:20 +0530

    fetch and update symbols db - incomplete

--- mysite/convert/views.py
+++ mysite/convert/views.py
@@ -1,7 +1,13 @@
+import requests, json
+
 from django.shortcuts import render
-from django.http import Http404
+from django.http import HttpResponse, Http404, JsonResponse
+from django.db import IntegrityError
+
+from mysite.settings import API_KEY
+
 from .forms import CurrencyConvertForm
-from .models import Currencies
+from .models import CountryCodes, Currencies
 
 
 # Create your views here.
@@ -28,3 +34,39 @@ def home(request):
 
     return render(request, template_name='/converter/home.html',
                   context={'form': form})
+
+
+def fetch_currency_symbols(request):
+    """GET view to fetch all currencies and tickers to populate db"""
+
+    # url_val to be made dynamic later -
+    url_val = 'http://data.fixer.io/api/symbols'
+    api_response = requests.get(url=url_val, params={'access_key': API_KEY})
+    # convert results to json (provided api provides results as json objects)
+    response = api_response.json()
+
+    if response['success']:
+        symbols = [_symb for _symb, _ in response['symbols'].items()]
+        currency = [_country for _, _country in response['symbols'].items()]
+
+        # save symbols and country to database
+        objs_list, boolean_list = [], []
+        for i_symbol, i_currency in zip(symbols, currency):
+            try:
+                obj, created = Currencies.objects.get_or_create(code=i_symbol,
+                                                                currency=i_currency,
+                                                                defaults={'currency': 'nameless currency'})
+                objs_list.append(obj)
+                boolean_list.append(created)
+            except IntegrityError:
+                # change to http reponse that says db is already populated
+                return JsonResponse({'created': False, 'country': i_symbol,
+                                     'currency': i_currency,
+                                     'error': 'Currency already present in database'})
+        # work with response data
+        saved_objects = [True for i_value in boolean_list if i_value]
+        return JsonResponse({'created': len(objs_list), 'saved': len(saved_objects)})
+
+    return HttpResponse(response)
+
+
