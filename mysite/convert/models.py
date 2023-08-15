67dbbb4c3b4002151c0e4ed2987e298674fdcfe8
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-15 14:08:59 +0530

    change model fields

--- mysite/convert/models.py
+++ mysite/convert/models.py
@@ -14,10 +14,18 @@ class CountryCodes(models.Model):
     def __str__(self):
         return self.currency
 
+    def get_currency_code_as_list(self):
+        return [self.code]
+
+    def get_currency_code(self):
+        return self.code
+
 
 class CurrencyConvert(models.Model):
     """db storing conversion values"""
-    from_currency = models.CharField(max_length=10)     # from_currency
+    currency = models.CharField(max_length=75)     # from_currency
+    code = models.CharField(max_length=3)
+
     to_USD = models.FloatField(default=0.0)     # conversion rate to USD for 1 from_currency
     to_EUR = models.FloatField(default=0.0)     # conversion rate to EUR for 1 from_currency
     to_GBP = models.FloatField(default=0.0)     # conversion rate to GBP for 1 from_currency
