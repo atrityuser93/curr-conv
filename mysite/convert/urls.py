56812131d035419659477324ed738dcef16b38a6
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-15 14:09:15 +0530

    show and delete exchange rates

--- mysite/convert/urls.py
+++ mysite/convert/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [path('', views.home, name='convert-home'),
                path('symbols/fetch/', views.fetch_currency_symbols, name='fetch-symbols'),
                path('symbols/delete/', views.delete_currency_symbols, name='delete-symbols'),
                path('symbols/show/', views.AvailableCurrencyListView.as_view(), name='symbol-list'),
+               path('rates/delete/', views.delete_exchange_rates, name='delete-rates'),
+               path('rates/show/', views.AvailableExchangeRatesView.as_view(), name='rates-list')
                ]
