0f5f205941b53ae811a2055d8082425f865a9c63
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-17 11:47:27 +0530

    move all operations to class methods
    move all conversion operation to class methods by creating a CurrencyConvert class. Cleans up view function and helps log each input request.

--- mysite/convert/models.py
+++ mysite/convert/models.py
@@ -1,4 +1,6 @@
-import datetime as dt
+from datetime import datetime
+import logging
+import requests
 
 from django.db import models
 from django.utils import timezone
@@ -26,8 +28,8 @@ class CountryCodes(models.Model):
 
 class ExchangeRates(models.Model):
     """db storing conversion values"""
-    currency = models.CharField(max_length=75)     # from_currency
     code = models.CharField(max_length=3)
+    currency = models.CharField(max_length=75)     # from_currency
 
     to_USD = models.FloatField(default=0.0)     # conversion rate to USD for 1 from_currency
     to_EUR = models.FloatField(default=0.0)     # conversion rate to EUR for 1 from_currency
@@ -37,6 +39,164 @@ class ExchangeRates(models.Model):
 
     def __str__(self):
         return self.currency
+
+    def generate_conversions(self, response, symbol: str):
+        """convert JSON response to values that could be used
+        for conversion between different currencies"""
+
+        if response["success"]:
+            usd_per_eur = response["rates"]["USD"]
+            gbp_per_eur = response["rates"]["GBP"]
+            jpy_per_eur = response["rates"]["JPY"]
+            sym_per_eur = response["rates"][symbol]
+            # convert to USD, GBP and JPY
+            usd__sym = usd_per_eur / sym_per_eur
+            gbp__sym = gbp_per_eur / sym_per_eur
+            jpy__sym = jpy_per_eur / sym_per_eur
+
+            # logging.info('generate_conversions: {} to EUR: {}'.format(symbol.code, 1 / sym_per_eur))
+            # logging.info('generate_conversions: USD per EUR: {}'.format(usd_per_eur))
+            # logging.info('generate_conversions: {} to USD: {}'.format(symbol.code, usd__sym))
+
+            self.to_USD = usd__sym
+            self.to_EUR = 1 / sym_per_eur
+            self.to_GBP = gbp__sym
+            self.to_JPY = jpy__sym
+            logging.info('generate_conversion: Update objects. Not Saved')
+
+        else:
+            logging.info('API call unsuccessful. Error: {} and Error code: {}'.format(response["error"]["info"],
+                                                                                      response["error"]["code"]))
+            # obj = ExchangeRates()
+            # logging.info('generate_conversion: Empty obj type {}'.format(type(obj)))
+        return self
+
+
+class CurrencyConvert(models.Model):
+    """db to store different conversions - db of records"""
+    input_currency = models.CharField(max_length=8)
+    output_currency = models.CharField(max_length=8)
+    input_value = models.FloatField(default=1.0)
+    output_value = models.FloatField(default=0.0)
+    conversion = models.FloatField(default=1.0)
+    is_converted = models.BooleanField(default=False)
+    asked_on = models.DateTimeField(default=timezone.now)
+
+    # self.convert()
+
+    def __str__(self):
+        return self.input_currency
+
+    def save(self, *args, **kwargs):
+        # self.input_currency = kwargs['input_currency_model'].code
+        # self.output_currency = kwargs['output_currency_model'].code
+        # logging.info('models: %s{} and %s{}'.format(self.input_currency, self.output_currency))
+        return super(CurrencyConvert, self).save(*args, **kwargs)
+
+    def convert(self, url, api_key):
+        """Implements currency conversion logic between input and output currencies"""
+        # fetch conversion rates
+        curr_in = CountryCodes.objects.all().get(pk=self.input_currency)
+        curr_out = CountryCodes.objects.all().get(pk=self.output_currency)
+        # logging.info('models: In type: {} Out type {}'.format(type(curr_in), type(curr_out)))
+        # logging.info('models: In: {}, Out: {}'.format(curr_in, curr_out))
+        currency_in, currency_out = self._get_conversion_rates(url=url, api_key=api_key)
+
+        # use EUR as base currency (Determined by API capabilities)
+        self.conversion = currency_in.to_EUR / currency_out.to_EUR
+
+        self.convert_currency()
+
+    def convert_currency(self):
+        """convert between currency values"""
+        self.output_value = self.conversion * self.input_value
+        self.is_converted = True
+
+    def converted(self):
+        return self.output_value
+
+    def _get_conversion_rates(self, url, api_key) -> (ExchangeRates, ExchangeRates):
+        """use fixer.io APIs to fetch latest conversion rates (once a day)"""
+
+        # logging.info('fetch conversion rates')
+        # get conversion value for given symbol within last one week
+        one_week = datetime.today() - timezone.timedelta(days=7)
+
+        currency_in = self._query_or_create(symbol=str(self.input_currency), updated_on=one_week,
+                                            url_val=url, api_key=api_key)
+        currency_out = self._query_or_create(symbol=str(self.output_currency), updated_on=one_week,
+                                             url_val=url, api_key=api_key)
+
+        # logging.info('Currency In type: {} Currency Out type: {}'.format(type(currency_in),
+        #                                                                  type(currency_out)))
+        # logging.info('Save object type 1: {} and 2: {}'.format(type(currency_in), type(currency_out)))
+        return currency_in, currency_out
+
+    def _query_or_create(self, symbol: str, updated_on: timezone.datetime,
+                         url_val: str, api_key: str) -> ExchangeRates:
+        """query db for existing value, if emtpy create new object and add to db"""
+
+        # check if currency exchange is in db
+        currency_query = ExchangeRates.objects.filter(code=symbol)
+        if currency_query:
+            # check if exchange rate is updated
+            currency_query_time = currency_query.filter(updated_on__gt=updated_on).order_by("updated_on")
+            logging.info('query_or_create (currency exists): currency_query_time {}'.format(type(currency_query_time)))
+
+            if currency_query_time:
+                # return relevant object if updated results are present
+                objs = currency_query_time.first()
+                logging.info('query_or_create (data exists): objs {}'.format(type(objs)))
+                return objs
+            else:
+                # return updated conversion object by getting updates through API call
+                response = self._request_api_call(url_val=url_val, symbol=symbol, api_key=api_key)
+                # symbol_obj = CountryCodes.objects.all().get(pk=symbol)
+                objs = currency_query.first()
+                objs = objs.generate_conversions(response, symbol=symbol)
+                # create empty exchange rate object here
+
+                # objs = ExchangeRates(code=symbol, currency=symbol_obj.currency)
+                # objs.conversions()
+                # objs = generate_conversions(response, symbol, old_obj=currency_query.values())
+                objs.save()  # save updated object
+                logging.info('query_or_create (data is old): objs {}'.format(type(objs)))
+                return objs
+
+        else:
+            # if query set is empty get latest conversion rate for
+            # non-existing row and add to db
+            logging.info('query_or_create(data does not exist) '
+                         'Creating new object for symbol {}'.format(symbol))
+            response = self._request_api_call(url_val=url_val, symbol=symbol, api_key=api_key)
+            # new ExchangeRates obj
+            curr_obj = CountryCodes.objects.all().get(pk=symbol)
+            objs = ExchangeRates(code=curr_obj.code, currency=curr_obj.currency)
+            response = self._request_api_call(url_val=url_val, symbol=curr_obj.code, api_key=api_key)
+            objs = objs.generate_conversions(response, symbol=curr_obj.code)
+            # currency_obj = create_convert_object(url_val, symbol)
+            # save new exchange obj
+            objs.save()
+            return objs
+
+    @staticmethod
+    def _request_api_call(url_val: str, symbol: str, api_key: str):
+        """call fixer.io api and get response as JSON objects.
+        Provides EUR (fixed base) to OTHER conversion rates.
+        Output is JSON"""
+
+        currency_list = ['USD', 'GBP', 'JPY', symbol]
+
+        # logging.info('Make API call for {}'.format(symbol))
+        # curr_obj = CountryCodes.objects.all().get(pk=symbol)
+        # all_symbols = currency_list + curr_obj.get_currency_code_as_list()
+        symbol_list = ','.join(currency_list)
+        # logging.info('{}'.format(symbol_list))
+        api_response = requests.get(url=url_val, params={'access_key': api_key,
+                                                         'symbols': symbol_list}
+                                    )
+        return api_response.json()
+
 
 
 
--- mysite/convert/views.py
+++ mysite/convert/views.py
@@ -13,7 +13,7 @@ from django.views.generic import ListView
 from mysite.settings import API_KEY
 
 from .forms import CurrencyConvertForm, CurrencyTickerDelete, CurrencyConvertDelete
-from .models import CountryCodes, ExchangeRates
+from .models import CountryCodes, ExchangeRates, CurrencyConvert
 
 
 # Create your views here.
@@ -27,21 +27,30 @@ def home(request):
             input_curr_symbol = form.cleaned_data['input_currency']
             output_curr_symbol = form.cleaned_data['output_currency']
             input_value = form.cleaned_data['input_value']
-            logging.info('Input currency: {} and Output currency: {}'.format(input_curr_symbol,
-                                                                             output_curr_symbol))
-            # currency conversion logic
-            # get db value for given -> USD
-            # conversion_to_usd = Currencies.object.all()
-            # get db value for USD -> output
-            conversion = fetch_conversion_rates(input_curr_symbol, output_curr_symbol)
-            # output_value = convert_currency(input_value, input_curr_symbol, output_curr_symbol)
-            output_value = input_value * conversion
+            logging.info('Input currency: {} and '
+                         'Output currency: {}'.format(input_curr_symbol,
+                                                      output_curr_symbol))
+            logging.info('Input type: {} Output type: {}'.format(type(input_curr_symbol),
+                                                                 type(output_curr_symbol)))
+            # create conversion object (for storing call in db)
+            conv_obj = CurrencyConvert(input_value=input_value,
+                                       input_currency=str(input_curr_symbol.code),
+                                       output_currency=str(output_curr_symbol.code),
+                                       )
+            conv_obj.convert(url='http://data.fixer.io/api/latest',
+                             api_key=API_KEY)
+            # logging.info('home: convert object input_currency: %s{}, '
+            #              'output_currency: %s{}'.format(conv_obj.input_currency,
+            #                                             conv_obj.output_currency))
+            # logging.info(conv_obj.input_currency)
+            # logging.info(conv_obj.output_currency)
+            conv_obj.save()
 
-            logging.info('{} {} is {} {}'.format(input_value, input_curr_symbol,
-                                                 output_value, output_curr_symbol))
+            logging.info('{} {} is {} {}'.format(conv_obj.input_value, conv_obj.input_currency,
+                                                 conv_obj.output_value, conv_obj.output_currency))
             # form = CurrencyConvertForm(data={'input_currency': CountryCodes.objects.all().get(pk=input_curr_symbol)})
             return render(request, template_name='converter/home.html',
-                          context={'form': form, 'complete': True, 'output_value': output_value})
+                          context={'form': form, 'complete': True, 'output_value': conv_obj.output_value})
 
     # get initial value for all form fields
 
@@ -61,11 +70,14 @@ def initial_form_value():
     return None
 
 
-def convert_currency(input_value, currency_in, currency_out):
-    """perform actual conversion between input and
-    output currency - given conversioon value"""
-    conversion = fetch_conversion_rates(currency_in, currency_out)
-    return None
+class ConvertCallsView(ListView):
+    """view all convert calls made so far"""
+    model = CurrencyConvert
+    template_name = 'converter/convert-list.html'
+    context_object_name = 'calls'
+
+    def get_queryset(self):
+        return super().get_queryset()
 
 
 def fetch_currency_symbols(request):
@@ -163,153 +175,6 @@ class AvailableExchangeRatesView(ListView):
     def get_queryset(self):
         return super().get_queryset()
     
-
-def fetch_conversion_rates(symbol_in: CountryCodes, symbol_out: CountryCodes):
-
-    # logging.info('fetch/update conversion rates symbol_in {} and symbol_out {}'.format(type(symbol_in),
-    #                                                                                    type(symbol_out)))
-    currency_in, currency_out = get_conversion_rates(symbol_in, symbol_out)
-    logging.info('currency in: {} and currency out: {}'.format(currency_in.code,
-                                                               currency_out.code))
-    # use EUR as base currency (Determined by API capabilities)
-    currency_in_2_out = currency_in.to_EUR/currency_out.to_EUR
-
-    logging.info('1 {} is {} {}'.format(currency_in.code,
-                                        currency_in_2_out,
-                                        currency_out.code))
-    return currency_in_2_out
-
-
-def get_conversion_rates(symbol_1: CountryCodes, symbol_2: CountryCodes) -> \
-        (ExchangeRates, ExchangeRates):
-    """use fixer.io APIs to fetch latest conversion rates (once a day)"""
-
-    # logging.info('fetch conversion rates')
-    # url_val to be made dynamic later -
-    url_val = 'http://data.fixer.io/api/latest'
-    # get conversion value for given symbol within last one week
-    # query_symbol = ExchangeRates.objects.filter(from_currency=symbol)
-    one_week = datetime.today() - timezone.timedelta(days=7)
-
-    currency_in = query_or_create(symbol=symbol_1, updated_on=one_week, url_val=url_val)
-    currency_out = query_or_create(symbol=symbol_2, updated_on=one_week, url_val=url_val)
-
-    # logging.info('Currency In type: {} Currency Out type: {}'.format(type(currency_in),
-    #                                                                  type(currency_out)))
-
-    # logging.info('Save object type 1: {} and 2: {}'.format(type(currency_in), type(currency_out)))
-    return currency_in, currency_out
-
-
-def query_or_create(symbol: CountryCodes, updated_on: timezone.datetime, url_val: str):
-    """query db for existing value, if emtpy create new object and add to db"""
-
-    # check if currency exchange is in db
-    currency_query = ExchangeRates.objects.filter(code=symbol.code)
-    if currency_query:
-        # check if exchange rate is updated
-        currency_query_time = currency_query.filter(updated_on__gt=updated_on).order_by("updated_on")
-        logging.info('query_or_create (currency exists): currency_query_time {}'.format(type(currency_query_time)))
-
-        if currency_query_time:
-            # return relevant object if updated results are present
-            objs = currency_query_time.first()
-            logging.info('query_or_create (data exists): objs {}'.format(type(objs)))
-            return objs
-
-        else:
-            # return updated conversion object by getting updates through API call
-            response = request_api_call(url_val=url_val, symbol=symbol)
-            objs = generate_conversions(response, symbol, old_obj=currency_query.values())
-            objs.save()         # save updated object
-            logging.info('query_or_create (data is old): objs {}'.format(type(objs)))
-            return objs
-
-    else:
-        # if query set is empty get latest conversion rate for
-        # non-existing row and add to db
-        logging.info('query_or_create(data does not exist) '
-                     'Creating new object for symbol {}'.format(symbol.code))
-        currency_obj = create_convert_object(url_val, symbol)
-        return currency_obj
-
-
-def create_convert_object(url_val: str, symbol: CountryCodes, currency_list=None) -> (ExchangeRates, bool):
-    """get new conversion values update database"""
-    response = request_api_call(url_val, symbol)
-    obj = generate_conversions(response, symbol)
-    obj.save()
-    # logging.info('Updated Object saved')
-    return obj
-
-
-# list of functions
-# request API and get response for EUR (fixed based) to OTHER conversion rates
-def request_api_call(url_val: str, symbol: CountryCodes):
-    """call fixer.io api and get response as JSON objects.
-    Provides EUR (fixed base) to OTHER conversion rates.
-    Output is JSON"""
-
-    currency_list = ['USD', 'GBP', 'JPY']
-
-    # logging.info('Make API call for {}'.format(symbol))
-    all_symbols = currency_list + symbol.get_currency_code_as_list()
-    symbol_list = ','.join(all_symbols)
-    # logging.info('{}'.format(symbol_list))
-    api_response = requests.get(url=url_val, params={'access_key': API_KEY,
-                                                     'symbols': symbol_list}
-                                )
-    return api_response.json()
-
-
-# convert response to storable value
-def generate_conversions(response, symbol: CountryCodes, old_obj: ExchangeRates = None) -> ExchangeRates:
-    """convert JSON response to values that could be used
-    for conversion between different currencies"""
-
-    if response["success"]:
-        usd_per_eur = response["rates"]["USD"]
-        gbp_per_eur = response["rates"]["GBP"]
-        jpy_per_eur = response["rates"]["JPY"]
-        sym_per_eur = response["rates"][symbol.code]
-        # convert to USD, GBP and JPY
-        usd__sym = usd_per_eur / sym_per_eur
-        gbp__sym = gbp_per_eur / sym_per_eur
-        jpy__sym = jpy_per_eur / sym_per_eur
-
-        logging.info('generate_conversions: {} to EUR: {}'.format(symbol.code, 1 / sym_per_eur))
-        logging.info('generate_conversions: USD per EUR: {}'.format(usd_per_eur))
-        logging.info('generate_conversions: {} to USD: {}'.format(symbol.code, usd__sym))
-
-        if old_obj is None:
-            # create ExchangeRates object w/o saving
-            obj = ExchangeRates(code=symbol.code,
-                                  currency=symbol.currency,
-                                  to_USD=usd__sym,
-                                  to_EUR=1/sym_per_eur,
-                                  to_GBP=gbp__sym,
-                                  to_JPY=jpy__sym)
-            # logging.info('generate_conversions: obj type {}'.format(type(obj)))
-            return obj
-        else:
-            # update existing ExchangeRates object
-            if old_obj.code == symbol.code and \
-                    old_obj.currency == symbol.currency:
-                old_obj.to_USD = usd__sym
-                old_obj.to_EUR = 1 / sym_per_eur
-                old_obj.to_GBP = gbp__sym
-                old_obj.to_JPY = jpy__sym
-                logging.info('generate_conversion: Update objects. Not Saved')
-            else:
-                logging.info('generate_conversion: Currency codes do not match. Will not update data.')
-            return old_obj
-    else:
-        logging.info('API call unsuccessful. Error: {} and Error code: {}'.format(response["error"]["info"],
-                                                                                  response["error"]["code"]))
-        obj = ExchangeRates()
-        logging.info('generate_conversion: Empty obj type {}'.format(type(obj)))
-        # return empty object
-        return obj
 
 # create object and save object to db
 # update existing object
