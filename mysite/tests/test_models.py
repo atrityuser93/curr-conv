f3feb0092fd87c86cb7e2763e2b820de30acb99b
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-19 22:31:31 +0530

    write first tests

--- mysite/tests/test_models.py
+++ mysite/tests/test_models.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from django.test import TestCase
 
-from convert.models import CountryCodes
+from convert.models import CountryCodes         # works only because of above hack changing sys.apth
 
 
 class CountryCodeTest(TestCase):
@@ -17,4 +17,12 @@ class CountryCodeTest(TestCase):
         curr = CountryCodes.objects.get(pk='ABX')
         max_length = curr._meta.get_field('currency').max_length
         self.assertEqual(max_length, 75)
+
+    def test_get_currency_code(self):
+        curr = CountryCodes.objects.get(pk='ABX')
+        self.assertEqual(curr.get_currency_code(), 'ABX')
+
+    def test_get_currency_name(self):
+        curr = CountryCodes.objects.get(pk='ABX')
+        self.assertEqual(curr.get_currency_name(), 'This is a test currency to test the model')
 
