8b06952eb237b4bc4d8851ba322efcef7ee631ba
atrityuser93 <141924408+atrityuser93@users.noreply.github.com>
2023-08-30 16:02:47 +0530

    remove unused form

--- mysite/convert/forms.py
+++ mysite/convert/forms.py
@@ -38,20 +38,6 @@ class CurrencyConvertForm(forms.Form):
     output_value = forms.FloatField(label='Amount', required=False)
 
 
-class CurrencyConvertDisplayForm(ModelForm):
-    """class used CurrencyConvert model to display results"""
-    class Meta:
-        model = CurrencyConvert
-        fields = ('input_currency', 'input_value', 'output_currency', 'output_value')
-        template_name = 'converter/'
-
-    def save(self, commit=True):
-        conv = super(CurrencyConvertDisplayForm, self).save(commit=False)
-        if commit:
-            conv.save()
-        return conv
-
-
 class CurrencyConvertDeleteForm(forms.Form):
     """delete all currency convert data"""
     delete_confirm = forms.BooleanField(label='confirm deletion',
@@ -59,38 +45,6 @@ class CurrencyConvertDeleteForm(forms.Form):
                                         help_text='Please confirm deletion',
                                         error_messages={'required': 'Please check box to confirm'},
                                         )
-# class ConvertForm(ModelForm):
-#     """model-based form for currency conversion"""
-#
-#     input_currency = forms.ModelChoiceField(queryset=CountryCodes.objects.all().order_by('currency'),
-#                                             required=True, help_text='Convert from',
-#                                             label='Convert from')
-#     input_value = forms.FloatField(label='Amount', required=True)
-#     output_currency = forms.ModelChoiceField(queryset=CountryCodes.objects.all().order_by('currency'),
-#                                              required=True, help_text='Convert to',
-#                                              label='Convert to')
-#
-#     class Meta:
-#         model = CurrencyConvert
-#         fields = ['input_currency', 'input_value', 'output_currency', 'output_value']
-#
-#     def save(self, commit=True):
-#         conv = super(ConvertForm, self).save(commit=False)
-#         conv.input_currency = self.cleaned_data["input_currency"].code
-#         conv.output_currency = self.cleaned_data["output_currency"].code
-#         logging.info('forms: conv obj type: {}'.format(type(conv)))
-#         # conv.convert
-#
-#         if commit:
-#             conv.save()
-#
-#         return conv
-
-#
-#     def save(self, commit=True):
-#         conv = super(ConvertForm, self).save(commit=False)
-#         conv.
-#
 
 
 
